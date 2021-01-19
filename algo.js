6f26fc6be95fecdd3cd3065639754f20c280d658
fjswayze <fjswayze@vassar.edu>
2021-01-19 09:35:52 -0800

    hjj

--- algo.js
+++ algo.js
@@ -528,4 +528,20 @@ var addBinary = function (a, b) {
 
 };
 
-console.log(addBinary("11", "1"))
+
+var climbStairs = function(n) {
+  if(n <= 0) return 0; 
+  if(n == 1) return 1; 
+  if(n == 2) return 2; 
+  let one_step_before = 2; 
+  let two_steps_before = 1; 
+  let all_ways = 0; 
+  for(let i = 2; i < n; i++){
+      all_ways = one_step_before + two_steps_before; 
+      two_steps_before = one_step_before; 
+      one_step_before = all_ways; 
+  }
+  return all_ways; 
+};
+
+console.log(climbStairs(5)); 
